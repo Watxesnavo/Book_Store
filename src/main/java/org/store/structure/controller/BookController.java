5e02143b806f0dcf44e9b4921ff67e340566d873
Watxesnavo <makasin_12@hotmail.com>
2023-09-22 22:01:11 +0200

    merge

--- src/main/java/org/store/structure/controller/BookController.java
+++ src/main/java/org/store/structure/controller/BookController.java
@@ -2,28 +2,18 @@ package org.store.structure.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> c47bcff (2nd part of project done.)
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.store.structure.dto.BookDto;
-<<<<<<< HEAD
 import org.store.structure.dto.BookSearchParametersDto;
 import org.store.structure.dto.CreateBookRequestDto;
 import org.store.structure.model.Book;
-=======
-import org.store.structure.dto.CreateBookRequestDto;
->>>>>>> c47bcff (2nd part of project done.)
 import org.store.structure.service.BookService;
 
 @RestController
@@ -46,7 +36,6 @@ public class BookController {
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
-<<<<<<< HEAD
 
     @PutMapping("/{id}")
     public BookDto updateBook(@PathVariable Long id, @RequestBody Book newBook) {
@@ -63,6 +52,4 @@ public class BookController {
         return bookService.searchBooks(searchParameters);
     }
 
-=======
->>>>>>> c47bcff (2nd part of project done.)
 }
