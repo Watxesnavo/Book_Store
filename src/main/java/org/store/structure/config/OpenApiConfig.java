b4591e91da5cd832455190482c198c08059afdf4
Watxesnavo <makasin_12@hotmail.com>
2023-09-26 21:28:11 +0200

    added: swagger pagination

--- src/main/java/org/store/structure/config/OpenApiConfig.java
+++ src/main/java/org/store/structure/config/OpenApiConfig.java
@@ -1,12 +1,11 @@
 package org.store.structure.config;
 
+import java.util.List;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
